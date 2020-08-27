add31ccc34658715b5e2d95d36f9e1710ab14f84
diegocastiblancob <diegocastiblancobuitrago@gmail.com>
2020-08-27 12:57:20 -0500

    Actualizacion con configuracion de hosting firebase

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,36 +7,46 @@ import { DashboardService } from '../../services/dashboard.service';
   providers: [DashboardService]
 })
 export class DashboardComponent implements OnInit {
-
+//declaracion de variables 
   public data;
   public articles;
   public categories;
   public categoryService;
   public testimonies;
   constructor(
+    //lamado del servicio
     private _serviceDashboard: DashboardService
   ) {
+    //inicializacion de metodos
     this.dataService();
   }
 
   ngOnInit(): void {
   }
 
-
-  showData(){
-    this.articles=this.data.result.articles_pymes_test;
-    this.categories=this.data.result.categories;
-    this.categoryService=this.categories[0].services;
-    this.testimonies=this.data.result.testimony;
+/**
+  * @param {String} metodo showDara
+  * @param {String} mensaje Cargar variables con los datos necesarios para ser mostrados
+  * @returns {Array} arrays cargados con  la informacion necesaria
+  */
+  showData() {
+    this.articles = this.data.result.articles_pymes_test;
+    this.categories = this.data.result.categories;
+    this.categoryService = this.categories[0].services;
+    this.testimonies = this.data.result.testimony;
   }
-
+/**
+  * @param {String} metodo dataService
+  * @param {String} mensaje llama el servicio consumido para tomar la informción del mismo
+  * @returns {Array} arrays con los datos del servicio
+  */
   dataService() {
     this._serviceDashboard.serviceNivelics().subscribe(
-      response=>{
-        this.data=response;
+      response => {
+        this.data = response;
         this.showData();
       },
-      error=>{
+      error => {
         console.log(<any>error);
       }
     );
--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../../services/dashboard.service';
-import {Filter} from './../../Models/filter';
+import { Filter } from './../../Models/filter';
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
@@ -9,7 +9,8 @@ import {Filter} from './../../Models/filter';
 
 })
 export class FilterComponent implements OnInit {
-public filter: Filter;
+  //declaracion de variables
+  public filter: Filter;
   public data;
   public articles;
   public categories;
@@ -18,29 +19,39 @@ public filter: Filter;
   public array;
 
   constructor(
+    //llamado del servicio
     private _serviceDashboard: DashboardService
   ) {
-    this.filter=new Filter(null);
+    //inicializacion e intanciamiento de variables, metodos y clases
+    this.filter = new Filter(null);
     this.dataService();
-    this.array=this.articles;
+    this.array = this.articles;
   }
 
   ngOnInit(): void {
   }
-
+  /**
+    * @param {String} metodo itemselected
+    * @param {String} mensaje valida el item seleccionado por el usuario
+    * @returns {Array} array con la informacion que el usuario selecciono
+    */
   itemSelected(item) {
-    if(item == 1){
-      this.array=this.articles;
+    if (item == 1) {
+      this.array = this.articles;
       console.log(this.array);
     }
-    if(item == 2){
-      this.array=this.categoryService;
+    if (item == 2) {
+      this.array = this.categoryService;
     }
-    if(item == 3){
-      this.array=this.testimonies;
+    if (item == 3) {
+      this.array = this.testimonies;
     }
   }
-
+  /**
+    * @param {String} metodo showDara
+    * @param {String} mensaje Cargar variables con los datos necesarios para ser mostrados
+    * @returns {Array} arrays cargados con  la informacion necesaria
+    */
   showData() {
     this.articles = this.data.result.articles_pymes_test;
     this.categories = this.data.result.categories;
@@ -48,14 +59,18 @@ public filter: Filter;
     this.testimonies = this.data.result.testimony;
   }
 
-
+  /**
+    * @param {String} metodo dataService
+    * @param {String} mensaje llama el servicio consumido para tomar la informción del mismo
+    * @returns {Array} arsays con los datos del servicio
+    */
   dataService() {
     this._serviceDashboard.serviceNivelics().subscribe(
-      response=>{
-        this.data=response;
+      response => {
+        this.data = response;
         this.showData();
       },
-      error=>{
+      error => {
         console.log(<any>error);
       }
     );
